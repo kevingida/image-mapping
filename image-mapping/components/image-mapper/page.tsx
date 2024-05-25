1c1f0ae2888f6d8e20cec10f35f6e112dd6e827c
kevingida <kevingida@gmail.com>
2024-05-25 16:38:40 +0200

    adjust image mapping

--- image-mapping/components/image-mapper/page.tsx
+++ image-mapping/components/image-mapper/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React, { useState } from "react";
 import ImageMapper from "react-image-mapper";
+import toast, { Toaster } from "react-hot-toast";
 
 const ImagesMapper = () => {
   const [msg, setMsg] = useState<string | null>(null);
@@ -13,107 +14,122 @@ const ImagesMapper = () => {
       {
         name: "office1",
         shape: "rect",
-        coords: [1261, 839, 1480, 1021],
-        preFillColor: "#5af3cd26",
+        coords: [1261, 839, 1480, 1026],
+        status: "done",
+        preFillColor: "#2bf3244c",
         fillColor: "#2bf3244c",
       },
       {
         name: "office2",
         shape: "rect",
-        coords: [1042, 839, 1260, 1025],
-        preFillColor: "#5af3cd26",
-        fillColor: "#2bf3244c",
+        coords: [1042, 839, 1260, 1026],
+        status: "undone",
+        preFillColor: "#f5001072",
+        fillColor: "#f5001072",
       },
       {
         name: "office3",
         shape: "rect",
-        coords: [305, 859, 523, 1025],
-        preFillColor: "#5af3cd26",
+        coords: [305, 859, 523, 1026],
+        status: "done",
+        preFillColor: "#2bf3244c",
         fillColor: "#2bf3244c",
       },
       {
         name: "office4",
         shape: "rect",
-        coords: [20, 542, 303, 785],
-        preFillColor: "#5af3cd26",
+        coords: [23, 542, 303, 785],
+        status: "done",
+        preFillColor: "#2bf3244c",
         fillColor: "#2bf3244c",
       },
       {
         name: "office5",
         shape: "rect",
-        coords: [29, 29, 239, 207],
-        preFillColor: "#5af3cd26",
+        coords: [23, 20, 239, 207],
+        status: "done",
+        preFillColor: "#2bf3244c",
         fillColor: "#2bf3244c",
       },
       {
         name: "office6",
         shape: "rect",
-        coords: [242, 29, 460, 211],
-        preFillColor: "#5af3cd26",
+        coords: [242, 20, 460, 211],
+        status: "done",
+        preFillColor: "#2bf3244c",
         fillColor: "#2bf3244c",
       },
       {
         name: "office7",
         shape: "rect",
-        coords: [462, 27, 678, 209],
-        preFillColor: "#5af3cd26",
+        coords: [462, 20, 678, 209],
+        status: "done",
+        preFillColor: "#2bf3244c",
         fillColor: "#2bf3244c",
       },
       {
         name: "office8",
         shape: "rect",
-        coords: [824, 23, 1040, 208],
-        preFillColor: "#5af3cd26",
-        fillColor: "#2bf3244c",
+        coords: [824, 20, 1040, 208],
+        status: "undone",
+        preFillColor: "#f5001072",
+        fillColor: "#f5001072",
       },
       {
         name: "boardroom",
         shape: "rect",
-        coords: [23, 787, 301, 1023],
-        preFillColor: "#5af3cd26",
+        coords: [23, 787, 303, 1026],
+        status: "done",
+        preFillColor: "#2bf3244c",
         fillColor: "#2bf3244c",
       },
       {
         name: "reception",
         shape: "rect",
         coords: [523, 1026, 1038, 760],
-        preFillColor: "#5af3cd26",
+        status: "done",
+        preFillColor: "#2bf3244c",
         fillColor: "#2bf3244c",
       },
       {
         name: "kitchen",
         shape: "rect",
-        coords: [1261, 20, 1477, 211],
-        preFillColor: "#5af3cd26",
+        coords: [1261, 20, 1480, 211],
+        status: "done",
+        preFillColor: "#2bf3244c",
         fillColor: "#2bf3244c",
       },
       {
         name: "toilet1",
         shape: "rect",
-        coords: [1043, 23, 1258, 209],
-        preFillColor: "#5af3cd26",
+        coords: [1043, 20, 1258, 209],
+        status: "done",
+        preFillColor: "#2bf3244c",
         fillColor: "#2bf3244c",
       },
       {
         name: "toilet2",
         shape: "rect",
         coords: [23, 353, 244, 540],
-        preFillColor: "#5af3cd26",
-        fillColor: "#2bf3244c",
+        status: "undone",
+        preFillColor: "#f5001072",
+        fillColor: "#f5001072",
       },
       {
         name: "garden",
         shape: "rect",
         coords: [415, 293, 1087, 757],
-        preFillColor: "#5af3cd26",
+        status: "done",
+        preFillColor: "#2bf3244c",
         fillColor: "#2bf3244c",
       },
       {
         name: "officePA",
         shape: "rect",
-        coords: [1475, 837, 1150, 209],
-        preFillColor: "#5af3cd26",
-        fillColor: "#2bf3244c",
+        coords: [1480, 837, 1150, 209],
+        status: "undone",
+        preFillColor: "#f5001072",
+        fillColor: "#f5001072",
       },
     ],
   };
@@ -126,6 +142,8 @@ const ImagesMapper = () => {
         area.coords
       )} !`
     );
+    if (area.status === "done") return toast.success(area.name);
+    if (area.status === "undone") return toast.error(area.name);
   };
 
   const clickedOutside = (evt) => {
@@ -194,6 +212,7 @@ const ImagesMapper = () => {
         <pre className="message">{msg ? msg : null}</pre>
         <pre>{moveMsg ? moveMsg : null}</pre>
       </div>
+      <Toaster position="top-center" reverseOrder={false} />
     </div>
   );
 };
